76178a94f6a342679b7a2b3bf5952a6125e35f34
Сергей Назаров <Tyuiu.NazarovSV@outlook.com>
2025-09-08 22:26:23 +0500

    Task1

---
+++ Tyuiu.NazarovSV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.NazarovSV.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int vozrast = 18;
+            string imya = "Сергей";
+            Console.WriteLine("Здравтсвуйте, меня зовут " + imya + ". Мне " + vozrast + " лет.");
+            Console.ReadKey();
+        }
+    }
+}
