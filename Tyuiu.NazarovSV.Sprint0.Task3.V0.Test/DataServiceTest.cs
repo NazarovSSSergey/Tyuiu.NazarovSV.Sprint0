6e329aedc892a8f1dff1684dcd3f6ab273abdb9f
Сергей Назаров <Tyuiu.NazarovSV@outlook.com>
2025-09-18 20:02:43 +0500

    Task3

---
+++ Tyuiu.NazarovSV.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.NazarovSV.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.NazarovSV.Task3.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckIfValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
