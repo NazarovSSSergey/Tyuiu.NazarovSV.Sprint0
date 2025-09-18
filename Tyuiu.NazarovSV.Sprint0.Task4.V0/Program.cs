a14ac33b91255cbfe511f33d2bfaa5bec9d9a847
Сергей Назаров <Tyuiu.NazarovSV@outlook.com>
2025-09-18 20:10:09 +0500

    Task4

---
+++ Tyuiu.NazarovSV.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.NazarovSV.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.NazarovSV.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(1, 5));
+            Console.ReadKey();
+        }
+    }
+}
